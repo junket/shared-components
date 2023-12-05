393b1a9594a203aa660b161ab72beb903315f12b
junket <789854+junket@users.noreply.github.com>
2023-12-05 07:39:54 -0500

    Adds the basics

---
+++ dist/use-uuids.esm.js
@@ -0,0 +1,2 @@
+function x(){return{createUUID:function(){var x=(new Date).getTime();return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,function(t){var r=(x+16*Math.random())%16|0;return x=Math.floor(x/16),("x"==t?r:3&r|8).toString(16)})}}}export{x as default};
+//# sourceMappingURL=use-uuids.esm.js.map
